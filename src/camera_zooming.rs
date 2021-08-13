39f56ceafde095614a5dae3f1e79afb63c401af2
Taryn Hill <taryn@phrohdoh.com>
2021-08-13 09:38:03 -0500

    update to bevy 0.5

--- src/camera_zooming.rs
+++ src/camera_zooming.rs
@@ -1,24 +1,20 @@
 use bevy::{input::mouse::MouseWheel, prelude::*, render::camera::Camera};
 
-#[derive(Default)]
-pub struct CameraZoomingState {
-    mouse_wheel_event_reader: EventReader<MouseWheel>,
-}
-
 const ZOOM_SENSITIVITY: f32 = 0.1;
 
 pub fn camera_zooming_system(
-    mut state: Local<CameraZoomingState>,
-    mouse_wheel: Res<Events<MouseWheel>>,
-    mut query: Query<(&Camera, &mut Scale)>,
+    mut mouse_wheel_event_reader: EventReader<MouseWheel>,
+    mut query: Query<(&Camera, &mut Transform)>,
 ) {
     let mut zoom_scalar = 1.0;
-    for mouse_wheel_event in state.mouse_wheel_event_reader.iter(&mouse_wheel) {
+    for mouse_wheel_event in mouse_wheel_event_reader.iter() {
         zoom_scalar *= 1.0 - ZOOM_SENSITIVITY * mouse_wheel_event.y;
     }
 
-    for (_, mut scale) in &mut query.iter() {
+    for (_, mut transform) in query.iter_mut() {
         // BUG: for some reason, when camera scale < 1.0, things just disappear!
-        *scale = Scale((scale.0 * zoom_scalar).max(1.0));
+        let zoomed = transform.scale * zoom_scalar;
+        let limited = Vec3::new(zoomed.x.max(1.0), zoomed.y.max(1.0), zoomed.z.max(1.0));
+        *transform.scale = *limited;
     }
 }
